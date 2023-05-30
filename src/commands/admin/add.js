46c5defdeb55ba4bb3ca2640c5c21b069417095c
EpicCoconutGuy <mjay.jirathanat@gmail.com>
2023-05-30 08:08:21 +0700

    update

--- src/commands/admin/add.js
+++ src/commands/admin/add.js
@@ -3,19 +3,54 @@ const {ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, SlashCommandB
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("add_homework")
-        .setDescription("Add new homework to list"),
-        async execute(interaction) {
+        .setDescription("Add new homework to list")
+        .addStringOption(option =>
+            option.setName("subject")
+                .setDescription("subject of new assignment")
+                .setRequired(true)
+                .addChoices(
+                    { name: "math", value: "math" },
+                    { name: "pysics", value: "pysics" },
+                    { name: "thai", value: "thai"},
+                ))
+        .addStringOption(option =>
+            option.setName("name")
+                .setDescription("name of assignment")
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName("description")
+                .setDescription("description of assignment (how to do)")
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName("page")
+                .setDescription("page of assignments")
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName("assigned")
+                .setDescription("assigned date of assignment (dd/mm/yyyy)")
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName("due")
+                .setDescription("due date of assignment (dd/mm/yyyy)")
+                .setRequired(true))
+
+        ,async execute(interaction) {
+            const subject_input = interaction.options.getString("subject")
+            const name_input = interaction.options.getString("name")
+            const description_input = interaction.options.getString("description")
+            const page_input = interaction.options.getString("page")
+
 
             const confirm_embed = new EmbedBuilder()
                 .setColor("Grey")
-                .setTitle("<homework_name>")
-                .setDescription("<page>")
+                .setTitle(`${subject_input} : ${name_input}`)
+                .setDescription(`${description_input}`)
                 .addFields(
+                    {name: "Page", value:`${page_input}`},
                     {name: "Assigned", value: "<assigned date (00-00-0000)>", inline: true},
                     {name: "Due", value: "<due date (00-00-0000)>", inline: true}
                 )
-                .setTimestamp()
-                .setFooter({text: "confirm data"});
+                .setTimestamp();
 
             const comfirm_button = new ButtonBuilder()
                 .setCustomId("confirm_button")
